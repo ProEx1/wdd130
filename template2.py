7c9931ccfb51e8404d4838eb758ad64e3a092cfc
ProEx1 <jjost1101@gmail.com>
2023-01-17 20:11:23 -0600

    1-17

---
+++ template2.py
@@ -0,0 +1,16 @@
+"""
+File: template2.py
+Author: Joshua Smith
+
+Purpose: Template2 for programs.
+"""
+
+dash = "------------------------------------------"
+print(dash)
+
+def main():
+    #Start here
+
+
+main()
+print(dash)
